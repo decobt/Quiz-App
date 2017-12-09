4f11807a1b9fad0ed94e2a452a59c45479351076
Trajche <roskoskitrajce@gmail.com>
2017-12-09 20:11:12 +0100

    added component to ensure security

---
+++ quiz-app/src/components/EnsureLoggedIn.js
@@ -0,0 +1,33 @@
+import { Component } from 'react';
+import {browserHistory} from 'react-router';
+
+//import connect
+import {connect} from 'react-redux';
+
+class EnsureLoggedIn extends Component {
+  componentDidMount() {
+    //check if user is logged in
+    if(!this.props.login){
+      //if not redirect to login form
+      browserHistory.replace('/login');
+    }else{
+      //if yes, redirect to dashboard
+      browserHistory.replace('/dashboard');
+    }
+  }
+
+  render() {
+    if(this.props.login){
+      return this.props.children;
+    }else{
+      return null;
+    }
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    login: state.loggedIn
+  }
+}
+export default connect(mapStateToProps)(EnsureLoggedIn)
