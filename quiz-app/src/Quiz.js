e9641a7152cd7851ce87c81adb0c80e2be633710
Trajche <roskoskitrajce@gmail.com>
2017-12-10 22:53:31 +0100

    changes to quiz styles

--- quiz-app/src/Quiz.js
+++ quiz-app/src/Quiz.js
@@ -117,9 +117,8 @@ class Quiz extends Component {
         return (
           <div className="container">
 
-          <div className="row" style={{background:'#333'}}>
-            <div className="col-xs-12 col-sm-8" style={{padding:'20px', color:'white', fontSize:'32px'}}>
-              <span>Quiz-Co App</span>
+          <div className="row">
+            <div className="col-xs-12 col-sm-8">
             </div>
             <div className="col-xs-3 col-sm-1 text-center create-button" data-toggle="collapse" href="#collapseExample">
               {this.state.score}
@@ -130,21 +129,26 @@ class Quiz extends Component {
             </div>
           </div>
 
-          <div className="row text-left" style={{background:'#f5f5f5'}}>
-            <div className="display" style={{padding:'50px 10px'}}>
-              <h1>Question {this.state.progress+1}: {prompt.question}</h1>
-            </div>
-            <form onSubmit={this.onSubmitQuestion}>
-              {options}
-            </form>
-          </div>
 
-          <div className="row" style={{background:'#333', padding:'20px 20px 0px'}}>
-              <div className="progress">
-                <div className="progress-bar  progress-bar-striped active" role="progressbar" aria-valuenow={this.state.progress} aria-valuemin="0" aria-valuemax="10" style={{width:pom+'%'}}>
+          <div className="row">
+            <p><strong>Question {this.state.progress+1} of 10</strong></p>
+              <div className="progress" style={{background:'#c0392b', height:'8px'}}>
+                <div className="progress-bar" role="progressbar" aria-valuenow={this.state.progress} aria-valuemin="0" aria-valuemax="10" style={{width:pom+'%',background:'white'}}>
                   <span className="sr-only">{pom}% Complete (danger)</span>
                 </div>
               </div>
+          </div>
+
+          <div className="row">
+            <div className="display text-center" style={{padding:'50px 10px', color:'#fefefe', background:'#c0392b', borderRadius:'3px'}}>
+              <h3>{prompt.question}</h3>
+            </div>
+          </div>
+
+          <div className="row">
+            <form onSubmit={this.onSubmitQuestion}>
+              {options}
+            </form>
           </div>
 
           </div>
@@ -168,14 +172,10 @@ class Option extends Component{
   render(){
     return (
       <div className="radio-boxes col-sm-6">
-        <div className="panel panel-primary">
-        <div className="panel-body answer">
           <label>
             <input type="radio" name="optionsRadios" id={this.props.data} value={this.props.data} onChange={this.props.onChangeFunc} />
             <span style={{fontSize:'16px', margin:'0px 20px'}}>{this.props.data}</span>
           </label>
-        </div>
-        </div>
       </div>
     );
   }
